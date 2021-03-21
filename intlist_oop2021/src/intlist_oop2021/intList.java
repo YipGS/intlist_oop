9eb09bb312e1624471d5d8a3722eb66b1bca6fde
YGS <YGS@192.168.0.172>
2021-03-21 19:10:02 +0100

    imutable intlist implementd using a linked list and node

--- intlist_oop2021/src/intlist_oop2021/intList.java
+++ intlist_oop2021/src/intlist_oop2021/intList.java
@@ -3,8 +3,12 @@ package intlist_oop2021;
 import java.util.Arrays;
 
 import javax.management.RuntimeErrorException;
+
+import intlist_oop.Node;
 //immutable first
 //1.make it immutable to start with, define the getters
+//implement after building a test, first create an invariant elements to store the list ---> getElements delete runtimeException, return elements.clone(), have to 
+//create a copy, which is the clone. --> implement constructor, 
 /**
  * 
  * Each instance of this class represents a sequence of int values.
@@ -15,13 +19,65 @@ import javax.management.RuntimeErrorException;
  *
  */
 public class intList {
+	
+	private static class Node {
+		private int value;
+		/**
+		 * @peerobject
+		 */
+		private Node next;
+		
+		
+		private static boolean hasLength(Node node, int length) {
+			if(length==0)
+				return node ==null;
+			else 
+				return length>0 && node!= null && hasLength(node.next, length-1);
+			
+			//return length == 0?node == null: length >0 &&node != null &&hasLength(node.next,length -1);	
+}
+	}
+	/**
+	 * @invar | Node.hasLength(head,length)
+	 */
+	private int length; 
+	private Node head; //first node is head 
+	/**
+	 * @invar |elements != null 
+	 * 
+	 * @representationObject 
+	 */
+	//representation is any object used by the class to help represent the abstract state of an instance of a class 
+	
+	//private int[] elements;
 	/** 
 	 * Returns an array containgthe  elements of this object
 	 * 
 	 * @creates | result
 	 * 
 	 */
-	public int[] getElements() {throw new RuntimeException("Not yet");}
+	public int[] getElements() {
+		int[] result =new int [length];
+		Node node = head;
+		int i=0;
+		while (node!= null) {
+			result[i++]=node.value;
+			node=node.next;
+		}
+		return result;
+	}
+	
+	private static Node createNodes(int[] elements, int index) {
+		if(index==elements.length)
+			return null;
+		else {
+			Node head = new Node();
+			head.value=elements[index];
+			head.next=createNodes(elements, index+1);
+			return head;
+		}
+	}
+	//if don't return a copy there will be a problem
 	
 	/**
 	 * Initializes this object so that it represents the sequence of int values contained in the given array
@@ -30,6 +86,7 @@ public class intList {
 	 * @post | Arrays.equals(getElements(),elements)
 	 */
 	public intList(int[] elements) {
-		throw new RuntimeException("Not yet");
+		this.head=createNodes(elements,0);
+		this.length=elements.length;
 	}
 }
